e487b177ddb4b06c408794b4a0ae30956eaca362
Morad Sleman <morad.sleman.a@gmail.com>
2020-07-21 10:51:16 +0300

    Auto stash before merge of "master" and "origin/master"

--- JS/Basic/morad/loops.js
+++
@@ -1,4 +0,0 @@
-var arr = [{name : 'sizar' , gender : 'male'},{name : 'saleh' , gender : 'male'},{name : 'moran' , gender : 'female'}]
-for(let i = 0 ; i <= arr.length - 1; i++){
-    console.log(`name ${arr[i].name} gender is : ${arr[i].gender}`);
-}
