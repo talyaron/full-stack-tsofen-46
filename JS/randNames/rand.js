e3019f3afa29009c4fceaf37ebe0f4abbb9eb363
Tal Yaron <tal.yaron@gmail.com>
2020-07-21 13:52:36 +0300

    cont

--- JS/randNames/rand.js
+++ JS/randNames/rand.js
@@ -1,56 +1,57 @@
 const names = [
-  "sally",
-  "lena",
-  "moran",
-  "monera",
-  "nivin",
-  "nimer",
-  "sizar",
-  "jeries",
-  "morad",
-  "moamen",
-  "mahmud",
-  "rawad",
-  "omri",
-  "saleh",
-  "taimaa",
-  "abdallah",
-  "yousef",
-  "marshood",
+    "sally",
+    "lena",
+    "moran",
+    "monera",
+    "nivin",
+    "nimer",
+    "sizar",
+    "jeries",
+    "morad",
+    "moamen",
+    "mahmud",
+    "rawad",
+    "omri",
+    "saleh",
+    "taimaa",
+    "abdallah",
+    "yousef",
+    "marshood",
 ];
 
 function randomNames(names, groupSize) {
-  try {
-    const groups = [];
+    try {
+        const groups = [];
+        const lengthNames=names.length / groupSize
 
-    for (let j = 0; j < names.length / groupSize; j++) {
-      const newGroup = [];
-      for (let i = 0; i < groupSize; i++) {
-        const indexOfName = getRandomName(names);
+        for (let j = 0; j < lengthNames; j++) {
+            const newGroup = [];
+            for (let i = 0; i < groupSize; i++) {
+                const indexOfName = getRandomName(names);
 
-        newGroup.push(names[indexOfName]);
-        names.splice(indexOfName, 1);
-      }
+                newGroup.push(names[indexOfName]);
+                names.splice(indexOfName, 1);
+            }
 
-      groups.push(newGroup);
+            groups.push(newGroup);
+        }
+
+        if (names.length > 0) {
+            groups.push(names);
+        }
+
+        console.log(groups);
+    } catch (err) {
+        console.error(err);
     }
-
-    if (names.length > 0) {
-      groups.push(names);
-    }
-
-    console.log(groups);
-  } catch (err) {
-    console.error(err);
-  }
 }
 
 function getRandomName(names) {
-  const arraySize = names.length;
+    const arraySize = names.length;
 
-  let indexOfName = Math.ceil(Math.random() * arraySize) - 1;
+    let indexOfName = Math.ceil(Math.random() * arraySize) - 1;
 
-  return indexOfName;
+    return indexOfName;
 }
 
-randomNames(names, 3);
+randomNames(names, 2);
