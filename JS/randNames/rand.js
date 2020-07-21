9903bc9f3acded30576e3d6727c5f5a3b5ab44f7
Nivin <nivin.abbas1@gmail.com>
2020-07-21 13:46:51 +0300

    update

--- JS/randNames/rand.js
+++ JS/randNames/rand.js
@@ -1,47 +1,56 @@
-const names = ['sally', 'lena', 'moran', 'monera'];
+const names = [
+  "sally",
+  "lena",
+  "moran",
+  "monera",
+  "nivin",
+  "nimer",
+  "sizar",
+  "jeries",
+  "morad",
+  "moamen",
+  "mahmud",
+  "rawad",
+  "omri",
+  "saleh",
+  "taimaa",
+  "abdallah",
+  "yousef",
+  "marshood",
+];
 
 function randomNames(names, groupSize) {
-    try {
+  try {
+    const groups = [];
 
-        const groups = []
+    for (let j = 0; j < names.length / groupSize; j++) {
+      const newGroup = [];
+      for (let i = 0; i < groupSize; i++) {
+        const indexOfName = getRandomName(names);
 
-        for (let j = 0; j < (names.length / groupSize); j++) {
-            
-            const newGroup = []
-            for (let i = 0; i < groupSize; i++) {
+        newGroup.push(names[indexOfName]);
+        names.splice(indexOfName, 1);
+      }
 
-                const indexOfName = getRandomName(names);
-
-
-                newGroup.push(names[indexOfName]);
-                names.splice(indexOfName, 1)
-
-
-            }
-
-
-            groups.push(newGroup);
-        }
-
-        if(names.length>0){groups.push(names)}
-
-        console.log(groups) 
-
-
-
-    } catch (err) {
-        console.error(err)
+      groups.push(newGroup);
     }
+
+    if (names.length > 0) {
+      groups.push(names);
+    }
+
+    console.log(groups);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function getRandomName(names) {
-    const arraySize = names.length;
+  const arraySize = names.length;
 
-    let indexOfName = Math.ceil(Math.random() * arraySize) - 1;
+  let indexOfName = Math.ceil(Math.random() * arraySize) - 1;
 
-
-
-    return indexOfName;
+  return indexOfName;
 }
 
-randomNames(names, 3)
+randomNames(names, 3);
