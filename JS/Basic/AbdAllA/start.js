3e5e957e303fe63be828df228ea13e238ec612e8
abdalla <abdalla.b.94@gmail.com>
2020-07-21 09:47:48 +0300

    start JS

---
+++ JS/Basic/AbdAllA/start.js
@@ -0,0 +1,15 @@
+let fn1='mrshood'
+let fn2='mhmood'
+
+let ln1='ayob'
+let ln2='ayob'
+
+let names={fn1:'jh', fn2:'jh' , age1:54 , age2:'5454'}
+
+if(names.age1 == names.age2 ){
+    console.log('it is true')
+}
+else{
+    console.log('NOT true ')
+}
+
