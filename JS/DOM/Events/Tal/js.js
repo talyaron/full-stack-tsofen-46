9dc59139aae37dc2f605498046f2cce408a7699c
Tal Yaron <tal.yaron@gmail.com>
2020-07-21 16:11:08 +0300

    running button

--- JS/DOM/Events/Tal/js.js
+++ JS/DOM/Events/Tal/js.js
@@ -2,4 +2,14 @@ console.dir(document)
 
 const button = document.getElementById('button');
 
-button.addEventListener('click',e=>{console.log(e)})
+button.addEventListener('mouseover', e => {
+    console.log(e)
+    const x= getNumber(200);
+    const y = getNumber(200);
+    button.style.top = `${y}px`;
+    button.style.left = `${x}px`;
+})
+
+function getNumber(max){
+    return Math.random()*max
+}
