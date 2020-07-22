03c675b6ab63d734552d7c733bc542d9125e212b
Tal Yaron <tal.yaron@gmail.com>
2020-07-22 14:28:48 +0300

    classes

---
+++ JS/Classes/Tal/js.js
@@ -0,0 +1,15 @@
+const element = document.getElementById('output');
+
+const salah = new Player('Salah', 10,element);
+const messi = new Player('Messi', 10,element);
+
+
+
+
+salah.sayHello();
+messi.sayHello();
+messi.setLastName = 'Lionel';
+messi.setPosion('strike')
+
+console.dir(messi);
+
