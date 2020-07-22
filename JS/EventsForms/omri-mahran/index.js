e4c38b7ba21284a7bce4ab1490dd7e14373f045a
Omri Zaher <omriza5@gmail.com>
2020-07-22 13:17:38 +0300

    modify: changed logic of index.js file

--- JS/EventsForms/omri-mahran/index.js
+++ JS/EventsForms/omri-mahran/index.js
@@ -1,21 +1,33 @@
+const state = {
+    name: "",
+    gender: "",
+    color: "",
+    date: "",
+}
+
 function handleSubmit(e) {
     e.preventDefault();
     const form = e.target;
 
-    const name = form[0].value;
-    const gender = form[1].value;
-    const color = form[2].value;
-    const date = new Date(form[3].value);
+    mapFormToState(form);
+    const date = new Date(state.date);
     const age = calculateAge(date);
 
+    const {
+        name,
+        gender,
+        color
+    } = state;
+
     const p = document.getElementById("mainPar");
+
     p.innerText = `Hello ${name},
     Welcome to our amazing site.
     your are a ${gender} :).
     and your age is ${age}`
 
     document.body.style.backgroundColor = color;
-    console.dir(form);
+
 }
 
 
@@ -24,4 +36,10 @@ function calculateAge(dob) {
     var age_dt = new Date(diff_ms);
 
     return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+function mapFormToState(form) {
+    for (let i = 0; i < form.length - 1; i++)
+        state[form[i].form[i].name] = form[i].form[i].value;
+
 }
