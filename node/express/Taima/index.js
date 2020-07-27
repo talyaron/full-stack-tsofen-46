3d4c86bb756b0814df8db98b222d7ea71745fa06
Taima <taimab98@gmail.com>
2020-07-27 19:24:23 +0300

    19

--- node/express/Taima/index.js
+++ node/express/Taima/index.js
@@ -1,30 +1,19 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
 
-app.get('/', function (req, res) {
-  res.send('Home')
+const jokes=[
+    "“Dad, I got my smarts from you, didn’t I?” - “That’s right my clever boy!” - “Yup, thought so, mom still has hers.”",
+    "I thought it would be nice to share a burger with this homeless guy I keep seeing on my way to work. - But that stingy jerk told me to go get my own.",
+    "What do electric cars and diarrhea have in common? - The idea, “I will make it home.”",
+    "“Dad, I got my smarts from you, didn’t I?” - “That’s right my clever boy!” - “Yup, thought so, mom still has hers.”"]
+
+app.use(express.static('public'))
+
+// let num = Math.floor((Math.random() * 3)
+
+app.get("/jokes", (req,res)=>{
+    res.send(jokes);
 })
 
-app.get('/about', function (req, res) {
-  res.send('about me')
-});
-
-app.get('/students/:studentID/:name?', function (req, res) {
-  const { studentID, name } = req.params
-  console.log(studentID, name)
-  res.send(`about the student with id: ${studentID} with name of ${name}`)
-})
-
-// app.get('/students/:studentID', function (req, res) {
-//   const { studentID } = req.params
-//   res.send(`about the student with id: ${studentID}`)
-// })
-
-
-
-
-app.get('/admin', function (req, res) {
-  res.send('admin panel')
-})
-
-app.listen(3000, () => { console.log('App listen on port 3000') })
+app.listen(3000, () =>  console.log('App listen on port 3000') )
