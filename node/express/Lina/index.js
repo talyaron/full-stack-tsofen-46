221fe54a37335a027d4b85541ba51010c53e3776
Lina <nijemlina@gmail.com>
2020-07-27 11:47:11 +0300

    public Lina

--- node/express/Lina/index.js
+++ node/express/Lina/index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express()
- 
+const fs = require('fs');
+
+
+app.use(express.static('public'));
+
+
 app.get('/', function (req, res) {
   res.send('Home')
 })
  
+
+
+
 app.get('/about', function (req, res) {
-  res.send('About me')
+  fs.readFile('./indext.html',{encoding:'utf-8'},(err,data)=>{
+    try{
+      res.send(data.toString());
+    }catch(err){
+      console.error(err);
+      res.status(404).send('Not found');
+    }
+  })
 })
 
 app.get('/students', function (req, res) {
@@ -16,4 +31,11 @@ app.get('/students', function (req, res) {
 app.get('/Admin', function (req, res) {
   res.send('About Admin')
 })
+
+app.get('/Studentd/:studentID/:name', function (req, res) {
+  const {studentID, name} = req.params
+  console.log(studentID, name)
+  res.send(`<h1>about the student with id: ${studentID} with name of ${name}</h1>`);
+})
+
 app.listen(3000, ()=>{console.log('App listen on port 3000')})
