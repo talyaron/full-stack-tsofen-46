99c41690b2ce2cc366a7a55ac830c5b0a636041d
Lina <nijemlina@gmail.com>
2020-07-27 10:34:19 +0300

    update express lina

--- node/express/Lina/index.js
+++ node/express/Lina/index.js
@@ -2,7 +2,18 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Home')
 })
  
+app.get('/about', function (req, res) {
+  res.send('About me')
+})
+
+app.get('/students', function (req, res) {
+  res.send('About student')
+})
+
+app.get('/Admin', function (req, res) {
+  res.send('About Admin')
+})
 app.listen(3000, ()=>{console.log('App listen on port 3000')})
