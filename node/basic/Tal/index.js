44d96c94aa4eaf8b4460eafed6f5f2e917fea601
Morad Sleman <morad.sleman.a@gmail.com>
2020-07-27 10:22:51 +0300

    Merge remote-tracking branch 'origin/master'

--- node/basic/Tal/index.js
+++ node/basic/Tal/index.js
@@ -6,5 +6,13 @@ if (m) { console.log('Sabach al noor') };
 if (a) { console.log('Ahalan') };
 if (e) {console.log('Masa al heir')};
 =======
+<<<<<<< HEAD
 console.log('hi')
+=======
+console.log('hi')
+function multi(x,y){
+    return x*y;
+}
+console.log(multi(2,3));
+>>>>>>> master
 >>>>>>> Stashed changes
