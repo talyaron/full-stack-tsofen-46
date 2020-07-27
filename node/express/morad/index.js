7fa76ba398bbb445707ee9130f93a71ad18d7869
Morad Sleman <morad.sleman.a@gmail.com>
2020-07-27 10:34:00 +0300

    express

--- node/express/morad/index.js
+++ node/express/morad/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-    res.send('Hello World')
+  res.send('Hello Worldsdfafnpsfsf')
 })
  
-app.listen(3000 , ()=>{console.log('App listen on port 3000')})
+app.listen(3000, ()=>{console.log('App listen on port 3000')})
