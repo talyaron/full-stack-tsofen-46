829c03dbee9804f928d301ed05a9bf1e107092c6
Mahmoud Ataria <mahmoud.ataria95@gmail.com>
2020-07-27 13:13:52 +0300

    new

---
+++ node/express/Jokes - ex/Rawad-Mahmoud/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+const students = [
+  "moran",
+ "monera",
+ "nivin",
+ "nimer",
+ "sizar",
+ "jeries",
+ "morad",
+ "rawad",
+ "omri",
+ "saleh",
+ "taimaa",
+ "abdallah",
+ "yousef",
+ "marshood",
+ "Rami",
+ "Maharn",
+ "sally"
+];
+
+//RERST api
+app.get('/students', function (req, res) {
+  
+  res.send(students)
+})
+
+
+
+app.get('/admin', function (req, res) {
+  res.send('admin panel')
+})
+
+app.listen(3000, () => { console.log('App listen on port 3000') })
