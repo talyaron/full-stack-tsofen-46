0b14fdf7d28e45ac7e0158134b331ed83fc885ec
Mahmoud Ataria <mahmoud.ataria95@gmail.com>
2020-07-28 09:47:13 +0300

    names

---
+++ node/Express-post/checkNames/Mahmoud/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+const jokes = [
+  
+ "Mahmoud",
+ "Marshood",
+ "Rawad",
+ "Tal"
+
+];
+
+function rand(){
+  
+  
+  const randNum = Math.floor(Math.random()*jokes.length);
+  console.log(randNum)
+  return randNum
+}
+
+
+//RERST api
+app.get('/jokes', function (req, res) {
+  // console.log("dasda:",students)
+  res.send({joke:jokes[rand()]})
+})
+
+
+
+
+app.get('/admin', function (req, res) {
+  res.send('admin panel')
+})
+
+app.listen(3000, () => { console.log('App listen on port 3000') })
