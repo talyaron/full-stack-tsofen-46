73995bac49f3d681273630e10b68ce5bbd1f0da5
Mahmoud Ataria <mahmoud.ataria95@gmail.com>
2020-07-28 14:27:39 +0300

    change

--- node/Express-post/login/Mahmoud/index.js
+++ node/Express-post/login/Mahmoud/index.js
@@ -4,45 +4,42 @@ const app = express()
 app.use(express.static('public'))
 
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
-})); 
+}));
 
-const Emailes = [
-  
- "Mahmoud.ataria95@gmail.com",
- "Marshood.ayoup@gmail.com",
- 
+const Emails = [
+  { email: "Mahmoud.ataria@gmail.com", pass: "1234" },
+  { email: "Marshood.ayoup@gmail.com", pass: "123" },
+
 ];
 
- 
+
 app.get('/names', function (req, res) {
-  
+
   res.send(names)
 })
 
-app.post('/names', (req, res)=>{
-  const {body} = req;
-  const {name} = body;
- var found=false;
+app.post('/names', (req, res) => {
+  const { body } = req;
+  const { name } = body;
+  const { Psw } = body;
+  var found = false;
 
 
-  for (i=0;i<=Emailes.length;i++){
-    if(Emailes[i] == name)
-    found=true;
-    
+  for (i = 0; i < Emails.length; i++) {
+    if (Emails[i].email == name && Emails[i].pass == Psw) {
+      found = true;
+    }
   }
 
-  if(found==true){
-    res.send({success:true})}
-  else{
-    res.send({success:false})}
-  
-if(name=='')
-{
-  res.send({'You need to enter your E-mail'})}
-}
+  if (found == true) {
+    res.send({ success: true })
+  }
+  else {
+    res.send({ success: false })
+  }
 
 })
 
