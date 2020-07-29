abcb165bd8b27f81635e25a152f71a039d5103b6
Tal Yaron <tal.yaron@gmail.com>
2020-07-29 13:21:55 +0300

    fix-bugs

--- node/express-update/Menu/Monera/public/functions.js
+++ node/express-update/Menu/Monera/public/functions.js
@@ -1,4 +1,4 @@
- 
+
 /*function renderMenu(menu) {
     
     console.log(name) 
@@ -20,28 +20,31 @@
 
 getMenu()
 
-function getMenu(){
+function getMenu() {
     fetch('/menu')
-    .then(res => res.json())
-    .then(data=>{
-        let items = '';
+        .then(res => res.json())
+        .then(data => {
+            let items = '';
 
-        console.log(data)
+            console.log(data)
 
-        data.forEach(item=>{
-            //${item.ID}")'>${item.name}, Price:${item.price} NIS
-            items += `<p> ID: ${item.ID} , Name: ${item.name} , Price: ${item.price} NIS , <label for="newPrice"> new Price: </label>, 
-            <input type="text" id='item.ID'> ,<button onclick="updatePrice(item.ID)">Update Price</button></p>`;
-        });
-     
-        const root = document.getElementById('root');
-        root.innerHTML = items
-    })
+            data.forEach(item => {
+                //${item.ID}")'>${item.name}, Price:${item.price} NIS
+                items += `<p> ID: ${item.ID} , Name: ${item.name} , Price: ${item.price} NIS , <label for="newPrice"> new Price: </label>, 
+            <input type="text" id='${item.ID}'><button onclick="updatePrice('${item.ID}')">Update Price</button></p>`;
+            });
+
+            const root = document.getElementById('root');
+            root.innerHTML = items
+        })
 }
 
-function updatePrice(Id){
-   const newPrice=document.getElementById("newPrice").value;
+function updatePrice(Id) {
+
     console.log(Id);
+
+    const newPrice = document.getElementById(Id).value;
+
     console.log(newPrice);
 
 }
