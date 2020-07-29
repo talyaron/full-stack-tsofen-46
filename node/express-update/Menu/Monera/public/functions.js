50036c4ec4648fc35a03c87a93cd730c32f6822d
Tal Yaron <tal.yaron@gmail.com>
2020-07-29 14:32:28 +0300

    cont

--- node/express-update/Menu/Monera/public/functions.js
+++ node/express-update/Menu/Monera/public/functions.js
@@ -22,44 +22,6 @@ getMenu()
 
 function getMenu() {
     fetch('/menu')
-<<<<<<< HEAD
-    .then(res => res.json())
-    .then(data=>{
-        let items = '';
-
-        console.log(data)
-
-        data.forEach(item=>{
-            //${item.ID}")'>${item.name}, Price:${item.price} NIS
-            items += `<p> ID: ${item.ID} , Name: ${item.name} , Price: ${item.price} NIS , <label for="newPrice"> new Price: </label>, 
-            <input type="text" id=${item.ID}> ,<button onclick="updatePrice('${item.ID}')">Update Price</button></p>`;
-        });
-     
-        const root = document.getElementById('root');
-        root.innerHTML = items
-    })
-}
-
-function updatePrice(Id){
-   const newPrice=document.getElementById(Id).value;
-   fetch('/priceUpdate', {
-    method: 'PUT',
-    body: JSON.stringify({Id,newPrice}),
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    })
-    
-    .then(res => res.json())
-    .then(data => {
-       
-        getMenu();
-        
-       
-    })
-   //console.log(Id);
-   // console.log(newPrice);
-=======
         .then(res => res.json())
         .then(data => {
             let items = '';
@@ -84,6 +46,5 @@ function updatePrice(Id) {
     const newPrice = document.getElementById(Id).value;
 
     console.log(newPrice);
->>>>>>> master
 
 }
