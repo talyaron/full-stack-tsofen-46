f441aae8e30c14d66876bf15274a2e1886871c87
Rami <ramiayoub12123@gmail.com>
2020-08-03 02:10:16 +0300

    Update

--- node/Promises/Rami/public/JS/functions.js
+++ node/Promises/Rami/public/JS/functions.js
@@ -8,9 +8,13 @@ function getItems(e){
         items.forEach(item => {
             itemslist += 
             `<div class='item'>
-            <img src='${item.URL}'>
-            <div class='topic'>${item.TOPIC}</div>
-            <div id='aa'>${item.PRICE}</div>
+            <img class='ItemImg' src='${item.URL}'>
+            <div class='topic'><h1>${item.TOPIC}</h1></div>
+            <div id='aa'><h2>Price: ${item.PRICE}</h2></div>
+                <div id='buttons'>
+                <button onclick='Edititem(${item.ID})' >Edit Item</button>
+                <button onclick='Deleteitem(${item.ID})' >Delete Item</button>
+                    </div>
             </div>`
           })
           document.getElementById("market").innerHTML = itemslist;
@@ -19,3 +23,30 @@ function getItems(e){
 
 }
 getItems(0);
+
+function Deleteitem(id){
+    let answer = window.confirm("Are You Sure Want To Delete Item?");
+    if(answer)
+    {
+        console.log("Del")
+        fetch('/api/Deleteitems', {
+            method: 'DELETE',
+            body: JSON.stringify({ id }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            const success = data;
+            if(success)
+            {
+                 getItems(0)
+            }else{
+                
+            }
+    
+    
+        })
+    }
+}
--- node/Promises/Rami/server.js
+++ node/Promises/Rami/server.js
@@ -11,7 +11,10 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(express.static('public'))
 
 let items=[
-    {ID:'1',TOPIC:'flower',URL:'img/1.jpg',PRICE:'5'}
+    {ID:'5',TOPIC:'flower',URL:'https://gardenseedsmarket.com/images/watermarked/5/detailed/54/010516.jpg',PRICE:'5'},
+    {ID:'2',TOPIC:'flower',URL:'img/1.jpg',PRICE:'5'},
+    {ID:'3',TOPIC:'flower',URL:'img/1.jpg',PRICE:'5'},
+    {ID:'4',TOPIC:'flower',URL:'img/1.jpg',PRICE:'5'}
 ]
 
 app.get('/api/items', (req, res) => {
@@ -24,6 +27,24 @@ app.get('/api/students', (req, res) => {
         res.send(students)
     },2000)
     
+})
+
+app.delete('/api/Deleteitems',(req,res)=>{
+    const {id} = req.body;
+    var found = 0;
+    items.forEach((elem,index) => {
+        if(elem.ID==id)
+        {
+            items.splice(index, 1)
+            found = 1;
+        }
+    });
+    if(found){
+        res.send(true);
+        console.log("im returning true")
+    }else{
+        res.send(false);
+    }
 })
 
 
