faa5fcaf3393587e861db6f403236ac3e63805c1
Tal Yaron <tal.yaron@gmail.com>
2020-08-04 13:55:09 +0300

    promise all

---
+++ node/Promises/Tal2/public/functions.js
@@ -0,0 +1,72 @@
+console.time('sync')
+// const getStudents = new Promise((resolve, reject) => {
+
+//     fetch('/api/students')
+//         .then(res => res.json())
+//         .then(data => {
+//             resolve(data)
+//         })
+// })
+
+// const getPupils = new Promise((resolve, reject) => {
+
+//     fetch('/api/pupils')
+//         .then(res => res.json())
+//         .then(data => {
+//             resolve(data)
+//         })
+// })
+
+function getPupilsImproved() {
+    return new Promise((resolve, reject) => {
+
+        fetch('/api/pupils')
+            .then(res => res.json())
+            .then(data => {
+                resolve(data)
+            })
+    })
+}
+
+//this is a one time promise, you get the first time data only!
+// getStudents.then(x => { console.log(x) });
+
+
+//this is a way to get many times the updated data.
+function getStudentImproved() {
+    return new Promise((resolve, reject) => {
+
+        fetch('/api/students')
+            .then(res => res.json())
+            .then(data => {
+                resolve(data)
+            })
+    })
+}
+
+//handle
+function handleGetStudent() {
+    // getStudents.then(x=>{console.log(x)});
+    getStudentImproved().then(x => { console.log(x) })
+}
+
+
+async function getAllData() {
+    console.time('sdfsdf')
+    let x = await getStudentImproved();
+    
+    console.log(x)
+    let y = await getPupilsImproved();
+    console.log(y)
+    console.timeEnd('sdfsdf')
+}
+
+getAllData();
+console.timeEnd('sync');
+
+// promise all
+// console.time('all')
+// Promise.all([getStudentImproved(), getPupilsImproved()]).then(results=>{
+//     console.log(results)
+//     console.timeEnd('all')
+// })
