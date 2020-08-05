19379a672d6b4c5fa63bc2985f00f5431661b932
abdalla <abdalla.b.94@gmail.com>
2020-08-05 14:35:08 +0300

    pull

--- node/Promises/abdalla.ex3/server.js
+++ node/Promises/abdalla.ex3/server.js
@@ -16,9 +16,9 @@ const resturant = [
     { name: 'Cola', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Coca-Cola_Glas_mit_Eis.jpg/280px-Coca-Cola_Glas_mit_Eis.jpg', price: 15, ID: uid() },
     { name: 'Knafe', img: 'https://turkishfoodchef.com/wp-content/uploads/2018/09/Knafeh-Kunefe-10.jpg', price: 15, ID: uid() }
 
-    
-
 ]
+
+
 
 function uid() {
 
@@ -26,7 +26,30 @@ function uid() {
 };
 
 
+const url = "mongodb+srv://abdalla1:abdalla99@cluster0.s25z3.mongodb.net/resturant";
+
+const mongoose = require('mongoose');
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Item = mongoose.model('Item', {
+    name: String,
+    img: img,
+    price :number 
+});
+
+const item1  = new Item({ name: 'Hamburger', img: 'https://www.kingsford.com/wp-content/uploads/2014/11/kfd-howtohamburger-Burgers_5_0391-1024x621.jpg',price:25 });
+const item1  = new Item({ name: 'mnsaf', img: 'https://www.kingsford.com/wp-content/uploads/2014/11/kfd-howtohamburger-Burgers_5_0391-1024x621.jpghttps://www.tareekaa.com/wp-content/uploads/2014/12/%D9%85%D9%86%D8%B3%D9%811.jpg',price:30 });
+const item1  = new Item({ name: 'maqlobe', img: 'https://kitchen.sayidaty.net/uploads/small/36/36fb0116ed3b918c96f3b1f3b3f1c333_w750_h750.jpg',price:36 });
+
+
+
+
+
+
+
+
 app.get('/resturant1', function (req, res) {
+
     res.send({ resturant });
 })
 
@@ -36,7 +59,7 @@ app.put('/resturant2', (req, res) => {
     const { itemID } = req.body;
 
     const index = resturant.findIndex(item => item.ID === itemID);
-    
+
     resturant[index].price = newprice;
 
 
@@ -48,36 +71,23 @@ app.put('/resturant3', (req, res) => {
     const { itemID } = req.body;
 
     const index = resturant.findIndex(item => item.ID === itemID);
-    
 
-    if(index!=-1){
-        resturant.splice(index,1)}
 
-     res.send({ resturant });
+    if (index != -1) {
+        resturant.splice(index, 1)
+    }
+
+    res.send({ resturant });
 
 })
 
 app.put('/resturant4', (req, res) => {
     const { itemID } = uid();
-    const {newprice} =req.body;
-    const{newimg}=req.body;
-    
-
-    resturant.push({ name:'newMeal' , img:newimg , price:newprice,ID:itemID});
-
-    res.send({ resturant });
-
-})
+    const { newprice } = req.body;
+    const { newimg } = req.body;
 
 
-
-
-
-app.put('/resturant5', ( req,res) => {
-
-   // const {resturant}=req.body;
-
-    resturant.sort(function(a, b){return a.price - b.price});
+    resturant.push({ name: 'newMeal', img: newimg, price: newprice, ID: itemID });
 
     res.send({ resturant });
 
@@ -85,6 +95,24 @@ app.put('/resturant5', ( req,res) => {
 
 
 
-app.listen(3335, () => { console.log("App is Listening") })
+
+
+app.put('/resturant5', (req, res) => {
+
+    // const {resturant}=req.body;
+
+    resturant.sort(function (a, b) { return a.price - b.price });
+
+    res.send({ resturant });
+
+})
+
+
+
+app.listen(3000, () => { console.log("App is Listening") })
+
+
+
+;
 
 
