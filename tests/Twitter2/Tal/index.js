d42fcc9be5957aa6f36dd319b3e6b78bcb618a93
Tal Yaron <tal.yaron@gmail.com>
2020-08-10 14:16:29 +0300

    cont

--- tests/Twitter2/Tal/index.js
+++ tests/Twitter2/Tal/index.js
@@ -9,12 +9,26 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.use(express.static('public'))
 
-const url = "mongodb+srv://lina:linanijem1@cluster0.mzrxp.mongodb.net/test";
+const url = "mongodb+srv://tal2:cFjgmJTT9b42Aoin@tal-test1-m39if.mongodb.net/test";
 
 //connection with DB
 const mongoose = require('mongoose');
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const User = mongoose.model('User', {
+    name: String,
+    imageUrl: String
+}); 
+
+app.post('/api/login',(req, res)=>{
+    const {name, imgUrl} = req.body;
+    
+    let user  = User({name, imgUrl})
+    user.save().then(doc=>{
+        console.log(doc)
+        res.send({login:true, id:doc._id})
+    })
+})
 
 const port = process.env.PORT || 3000;
 
