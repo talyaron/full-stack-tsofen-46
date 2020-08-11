a0c5ec74c88329970c6f1043439f423b8d5454a6
Saleh <salehao1996@gmail.com>
2020-08-11 15:59:59 +0300

    addded

---
+++ React/01-basic/Saleh/basic/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+import Nav from './view/components/Nav/Nav'
+
+function App() {
+  return (
+    <Nav></Nav>
+  );
+}
+
+export default App;
