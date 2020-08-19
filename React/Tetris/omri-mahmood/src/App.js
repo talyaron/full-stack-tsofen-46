162c8da361705df8f952573b43e065e61e8a8d53
Omri Zaher <omriza5@gmail.com>
2020-08-19 11:13:18 +0300

    some changes

--- React/Tetris/omri-mahmood/src/App.js
+++ React/Tetris/omri-mahmood/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
@@ -16,7 +16,7 @@ function App() {
     const obj = { ...imgObj };
     obj[name] = e.currentTarget.src;
 
-    setResult([...result, obj]);
+    setResult([obj, ...result]);
   };
   console.log(result);
   return (
@@ -61,50 +61,41 @@ function App() {
           </th>
         </tr>
 
-        {result
-          .slice(0)
-          .reverse()
-          .map((img) => (
-            <tr>
-              {img.cake ? (
-                <td>
-                  <img src={img.cake} className="candy-img"></img>
-                </td>
-              ) : (
-                <td></td>
-              )}
-              {img.combo ? (
-                <td>
-                  <img src={img.combo} className="candy-img"></img>
-                </td>
-              ) : (
-                <td></td>
-              )}
-              {img.circle ? (
-                <td>
-                  <img src={img.circle} className="candy-img"></img>
-                </td>
-              ) : (
-                <td></td>
-              )}
-              {img.pink ? (
-                <td>
-                  <img src={img.pink} className="candy-img"></img>
-                </td>
-              ) : (
-                <td></td>
-              )}
-            </tr>
-          ))}
+        {result.map((img) => (
+          <tr>
+            {img.cake ? (
+              <td>
+                <img src={img.cake} className="candy-img"></img>
+              </td>
+            ) : (
+              <td></td>
+            )}
+            {img.combo ? (
+              <td>
+                <img src={img.combo} className="candy-img"></img>
+              </td>
+            ) : (
+              <td></td>
+            )}
+            {img.circle ? (
+              <td>
+                <img src={img.circle} className="candy-img"></img>
+              </td>
+            ) : (
+              <td></td>
+            )}
+            {img.pink ? (
+              <td>
+                <img src={img.pink} className="candy-img"></img>
+              </td>
+            ) : (
+              <td></td>
+            )}
+          </tr>
+        ))}
       </table>
     </div>
   );
 }
 
 export default App;
-
-//https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTN9LcnXGHPd1A3Rl5QRA_GG8p0OVqROR9R2w&usqp=CAU
-//https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSqwAdOx9rAVWB4Lq_JG4srbDEIune6aDzA7g&usqp=CAU
-//https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRDn_DWDbXxQc4OLHeWj79701LdDwXihXqBg&usqp=CAU
-//https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTzXE_LTFsVZnRdQac8um8T9LA6LT-HE1qBqA&usqp=CAU
-//https://image.winudf.com/v2/image1/Y29tLmJyaWdodC5saWdodC5icmlnaHRsaWdodF9zY3JlZW5fMF8xNTQ0NTA3MzA3XzAxNA/screen-0.jpg?fakeurl=1&type=.jpg
